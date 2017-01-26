8c59077ebd0639bac52ba24c3116a881b4d8c78c
Tony Morinello <morinelloa@gmail.com>
2017-01-26 08:34:10 -0500

    Scoring stubbing complete
    Still need the following:
    -Override Equals, ToString, and GetHashcode methods
    -Testing for all classes

--- DualMeetManager/DualMeetManager/Domain/Scoring/OverallScore.cs
+++ DualMeetManager/DualMeetManager/Domain/Scoring/OverallScore.cs
@@ -8,7 +8,23 @@ namespace DualMeetManager.Domain.Scoring
 {
     class OverallScore
     {
-        Dictionary<string, List<IndEvent>> individualEvents;
-        Dictionary<string, List<IndEvent>> relayEvents;
+        //Team1 vs Team2, <Team1 pts, Team2 pts>
+        Dictionary<string, Tuple<decimal, decimal>> overallPts;
+        //Event name, List of points
+        Dictionary<string, List<RunningEvent>> runningEvents;
+        Dictionary<string, List<FieldEvent>> throwingEvents;
+        Dictionary<string, List<RelayEvent>> relayEvents;
+
+        //Defult Constructor
+        public OverallScore(){}
+
+        //Parameterized Constructor
+        public OverallScore(Dictionary<string, Tuple<decimal, decimal>> overallPts, Dictionary<string, List<RunningEvent>> runningEvents, Dictionary<string, List<FieldEvent>> throwingEvents, Dictionary<string, List<RelayEvent>> relayEvents)
+        {
+            this.overallPts = overallPts;
+            this.runningEvents = runningEvents;
+            this.throwingEvents = throwingEvents;
+            this.relayEvents = relayEvents;
+        }
     }
 }
---
+++ DualMeetManager/DualMeetManager/Domain/Scoring/RunningEvent.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DualMeetManager.Domain.Scoring
+{
+    class RunningEvent
+    {
+        //Teams
+        //Abbr, Full Name
+        Tuple<string, string> team1 { get; set; }
+        Tuple<string, string> team2 { get; set; }
+
+        //Pts
+        //Team1 pts, Team2 pts, athlete name, school name, performance
+        //Note: performance is a string because it could be in minutes and second (ex: 4:25)
+        Tuple<decimal, decimal, string, string, string> firstPlacePts { get; set; }
+        Tuple<decimal, decimal, string, string, string> secondPlacePts { get; set; }
+        Tuple<decimal, decimal, string, string, string> thirdPlacePts { get; set; }
+
+        //Total
+        //Team1 total, Team2 total
+        Tuple<decimal, decimal> totalPts { get; set; }
+
+        //Default Constructor
+        public RunningEvent() { }
+
+        //Constructor without points
+        public RunningEvent(Tuple<string, string> team1, Tuple<string, string> team2)
+        {
+            this.team1 = team1;
+            this.team2 = team2;
+        }
+
+        //Constructor without points without using tuples in parameters
+        public RunningEvent(string team1Abbr, string team1Name, string team2Abbr, string team2Name)
+        {
+            team1 = Tuple.Create(team1Abbr, team1Name);
+            team2 = Tuple.Create(team2Abbr, team2Name);
+        }
+
+        //Constructor with points
+        public RunningEvent(Tuple<string, string> team1, Tuple<string, string> team2, Tuple<decimal, decimal, string, string, string> firstPlacePts, Tuple<decimal, decimal, string, string, string> secondPlacePts, Tuple<decimal, decimal, string, string, string> thirdPlacePts, Tuple<decimal, decimal> totalPts)
+        {
+            this.team1 = team1;
+            this.team2 = team2;
+            this.firstPlacePts = firstPlacePts;
+            this.secondPlacePts = secondPlacePts;
+            this.thirdPlacePts = thirdPlacePts;
+            this.totalPts = totalPts;
+        }
+    }
+}
