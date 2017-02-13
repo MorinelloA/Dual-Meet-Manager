c1bf05752eba8ceda629a0456e293f733e472329
Tony Morinello <morinelloa@gmail.com>
2017-02-13 00:04:30 -0500

    Business Layer Managers

--- DualMeetManager/DualMeetManager/Business/Managers/EventMgr.cs
+++ DualMeetManager/DualMeetManager/Business/Managers/EventMgr.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DualMeetManager.Business.Exceptions;
+using DualMeetManager.Service.DataEntry;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,74 @@ using System.Threading.Tasks;
 
 namespace DualMeetManager.Business.Managers
 {
-    class EventMgr
+    public class EventMgr : Manager
     {
+        public string ConvertToTimedData(decimal perf)
+        {
+            try
+            {
+                IDataEntrySvc dataEntrySvc = (IDataEntrySvc)GetService(typeof(IDataEntrySvc).Name);
+                string timedData = dataEntrySvc.ConvertToTimedData(perf);
+                return timedData;
+            }
+            catch (InvalidPerformanceException ipe)
+            {
+                Console.WriteLine("Error converting " + perf + " to timed data");
+                Console.WriteLine(ipe.ToString());
+                Console.Write(ipe.StackTrace);
+                return "";
+            }
+        }
+
+        public decimal ConvertFromTimedData(string perf)
+        {
+            try
+            {
+                IDataEntrySvc dataEntrySvc = (IDataEntrySvc)GetService(typeof(IDataEntrySvc).Name);
+                decimal rawData = dataEntrySvc.ConvertFromTimedData(perf);
+                return rawData;
+            }
+            catch (InvalidPerformanceException ipe)
+            {
+                Console.WriteLine("Error converting " + perf + " to raw data");
+                Console.WriteLine(ipe.ToString());
+                Console.Write(ipe.StackTrace);
+                return 0m;
+            }
+        }
+
+        public string ConvertToLengthData(decimal perf)
+        {
+            try
+            {
+                IDataEntrySvc dataEntrySvc = (IDataEntrySvc)GetService(typeof(IDataEntrySvc).Name);
+                string lengthData = dataEntrySvc.ConvertToLengthData(perf);
+                return lengthData;
+            }
+            catch (InvalidPerformanceException ipe)
+            {
+                Console.WriteLine("Error converting " + perf + " to length data");
+                Console.WriteLine(ipe.ToString());
+                Console.Write(ipe.StackTrace);
+                return "";
+            }
+        }
+
+        public decimal ConvertFromLengthData(string perf)
+        {
+            try
+            {
+                IDataEntrySvc dataEntrySvc = (IDataEntrySvc)GetService(typeof(IDataEntrySvc).Name);
+                decimal rawData = dataEntrySvc.ConvertFromLengthData(perf);
+                return rawData;
+            }
+            catch (InvalidPerformanceException ipe)
+            {
+                Console.WriteLine("Error converting " + perf + " to raw data");
+                Console.WriteLine(ipe.ToString());
+                Console.Write(ipe.StackTrace);
+                return 0m;
+            }
+        }
     }
 }
--- DualMeetManager/DualMeetManager/Business/Managers/MeetMgr.cs
+++ DualMeetManager/DualMeetManager/Business/Managers/MeetMgr.cs
@@ -1,12 +1,39 @@
-﻿using System;
+﻿using DualMeetManager.Domain;
+using DualMeetManager.Service.Saving;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace DualMeetManager.Business.Managers
 {
-    class MeetMgr
+    public class MeetMgr : Manager
     {
+        public Meet openMeet(string filePath)
+        {
+            ISavingSvc saveSvc = (ISavingSvc)GetService(typeof(ISavingSvc).Name);
+            Meet meetToOpen = saveSvc.openMeet(filePath);
+            if (meetToOpen == null)
+            {
+                MessageBox.Show("Meet did NOT open correctly!");
+            }
+            else
+            {
+                MessageBox.Show("Meet opened correctly!");
+            }
+            return meetToOpen;
+        }
+
+        public void saveMeet(string filePath, Meet meetToSave)
+        {
+            ISavingSvc saveSvc = (ISavingSvc)GetService(typeof(ISavingSvc).Name);
+            bool didSave = saveSvc.saveMeet(filePath, meetToSave);
+            if (!didSave)
+                MessageBox.Show("Meet did NOT save!");
+            else
+                MessageBox.Show("Meet saved correctly - " + filePath);
+        }
     }
 }
